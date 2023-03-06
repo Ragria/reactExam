d0c843bc5ae2721f11e54e23646767f36cad410d
Ahn <asy3310@gmail.com>
2023-03-06 12:10:35 +0900

    애플코딩 무료강의 exam1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
+import React, { useState } from 'react'; 
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let posts = '고기'
+
+  let [title, titleModi] = useState(['남자 코트 추천1', '남자 코트 추천2', '남자 코트 추천']); //변수 대신 쓸수 있는 저장공간 import 필요
+  let [good, goodModi] = useState(0);
+
+  function titleModify(){
+    var newArray = [...title]; // deepCopy
+    newArray[0] = '여자 코트 추천'
+
+    titleModi(newArray);
+  }
+
+  // function titleModify(){
+  //   var newArray = [...title]; // deepCopy
+  //   newArray.sort()
+
+  //   titleModi(newArray);
+  // }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>리액트 연습</div>
+      </div>
+      <button onClick={titleModify}>버튼</button>
+      <div className="list">
+        <h3>{title[0]} <span onClick={()=>{goodModi(good + 1)}}>👍</span> {good} </h3>
+        <p>2월 17일 방행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3>{title[1]}</h3>
+        <p>2월 17일 방행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3>{title[2]}</h3>
+        <p>2월 17일 방행</p>
+        <hr/>
+      </div>
+
+      <Modal/>
+      
     </div>
   );
+}
+
+
+function Modal() { //관습 Component는 대문자부터 시작
+  return(
+    <> 
+    <div className='modal'>
+      <h2>제목</h2>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+    </>
+  )
 }
 
 export default App;
